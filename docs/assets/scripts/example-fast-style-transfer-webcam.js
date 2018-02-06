5d66818f6b731607755cd6994982db2353e1dc54
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-02-06 16:19:06 -0500

    Deploy website
    Deploy website version based on 780046bb8365923d982b03ab5eb478e161549030

---
+++ docs/assets/scripts/example-fast-style-transfer-webcam.js
@@ -0,0 +1,44 @@
+/*
+===
+Fast Style Transfer Mirror Example
+===
+*/
+
+let net;
+let outputImgData;
+let outputImg;
+let outputImgContainer;
+let video;
+let modelReady = false;
+let startPredict = false;
+
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(200, 200);
+  video.hide();
+  net = new ml5.TransformNet('assets/models/udnie', modelLoaded);
+  outputImgContainer = createImg('assets/img/udnie.jpg', 'image');
+  outputImgContainer.parent('output-container');
+}
+
+function draw() {
+  if (startPredict && modelReady) {
+    predict();
+  }
+}
+
+function modelLoaded() {
+  console.log('model loaded');
+  modelReady = true;
+}
+
+function togglePredicting() {
+  startPredict = !startPredict;
+}
+
+function predict() {
+  outputImgData = net.predict(video.elt);
+  outputImg = ml5.array3DToImage(outputImgData);
+  outputImgContainer.elt.src = outputImg.src;
+}
