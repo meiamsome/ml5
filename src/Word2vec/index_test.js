9d82c6c8fd6a2468a1d08164ad7e20b10ae236a3
Oliver Wright <meiamsome@meiamso.me>
2018-06-26 03:37:52 +0100

    Re-enable word2vec tests

--- src/Word2vec/index_test.js
+++ src/Word2vec/index_test.js
@@ -1,19 +1,17 @@
-// import Word2Vec from './index';
+import Word2Vec from './index';
 
-// const URL = 'https://raw.githubusercontent.com/ml5js/ml5-examples/master/p5js/07_Word2Vec/data/wordvecs1000.json';
+const URL = 'https://raw.githubusercontent.com/ml5js/ml5-examples/master/p5js/Word2Vec/data/wordvecs1000.json';
 
-// describe('initialize word2vec', () => {
-//   let word2vec;
-//   beforeAll((done) => {
-//     // word2vec = new Word2Vec(URL);
-//     done();
-//   });
+describe('initialize word2vec', () => {
+  let word2vec;
+  beforeAll((done) => {
+    word2vec = Word2Vec(URL, done);
+  });
 
-//   // it('creates a new instance', (done) => {
-//   //   expect(word2vec).toEqual(jasmine.objectContaining({
-//   //     ready: true,
-//   //     modelSize: 1,
-//   //   }));
-//   //   done();
-//   // });
-// });
+  it('creates a new instance', () => {
+    expect(word2vec).toEqual(jasmine.objectContaining({
+      ready: true,
+      modelSize: 1,
+    }));
+  });
+});
